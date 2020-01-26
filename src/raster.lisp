ddb30e1d41000fdb7c50d1af0411d38d798c6f92
erik <erik@cadlag.org>
2020-01-25 20:25:29 -0800

    matrices + homogenous coordinates

--- src/raster.lisp
+++ src/raster.lisp
@@ -1,39 +1,5 @@
 (in-package :rastaman)
 
-(defparameter *stroke-color* (list 255 255 255))
-
-(defun %draw-line-by-x-coords (image x0 y0 x1 y1)
-  (assert (<= x0 x1))
-  (let* ((dx (- x1 x0))
-         (dy (- y1 y0))
-         (derror2 (* 2 (abs dy)))
-         (ystep (if (> y1 y0) 1 -1)))
-    (loop :with y := y0
-          :with error2 := 0
-          :for x :from x0 :upto x1
-          :do (set-pixel-color! image x y *stroke-color*)
-          :do (progn
-                (incf error2 derror2)
-                (when (> error2 dx)
-                  (incf y ystep)
-                  (decf error2 (* 2 dx)))))))
-
-(defun %draw-line-by-y-coords (image x0 y0 x1 y1)
-  (assert (<= y0 y1))
-  (let* ((dy (- y1 y0))
-         (dx (- x1 x0))
-         (derror2 (* 2 (abs dx)))
-         (xstep (if (> x1 x0) 1 -1)))
-    (loop :with x := x0
-          :with error2 := 0
-          :for y :from y0 :upto y1
-          :do (set-pixel-color! image x y *stroke-color*)
-          :do (progn
-                (incf error2 derror2)
-                (when (> error2 dy)
-                  (incf x xstep)
-                  (decf error2 (* 2 dy)))))))
-
 ;;; TODO: flip y coord
 ;;; TODO: clip to screen
 
@@ -42,38 +8,41 @@
 (defvar *world->screen*)
 (defvar *draw-color*)
 
+(defvar *viewport-matrix*)
+(defvar *projection-matrix*)
+(defvar *modelview-matrix*)
+
 (defun draw-triangle (image a b c)
   ;; Note: now a,b,c are vec3f
   (check-type a vec3)
   (check-type b vec3)
   (check-type c vec3)
-  (let ((pa (funcall *world->screen* a))
-        (pb (funcall *world->screen* b))
-        (pc (funcall *world->screen* c)))
-    (flet ((draw-point (x y)
-             (multiple-value-bind (q r s)
-                 (barycentric-coordinates (vec2 x y) pa pb pc)
-               (unless (or (minusp q) (minusp r) (minusp s))
-                 ;; inside triangle, so draw it
-                 (let ((z (+ (* q (vz3 a))
-                             (* r (vz3 b))
-                             (* s (vz3 c)))))
-                   (when (> z (aref *z-buffer* x y))
-                     (setf (aref *z-buffer* x y) z)
-                     (set-pixel-color! image x y *draw-color*)))))))
-      (let ((min-x (truncate (min (vx2 pa) (vx2 pb) (vx2 pc))))
-            (max-x (truncate (max (vx2 pa) (vx2 pb) (vx2 pc))))
-            (min-y (truncate (min (vy2 pa) (vy2 pb) (vy2 pc))))
-            (max-y (truncate (max (vy2 pa) (vy2 pb) (vy2 pc)))))
-        (loop :for x :from min-x :upto max-x
-              :do (loop :for y :from min-y :upto max-y
-                        :do (draw-point x y)))))))
+  (flet ((draw-point (x y)
+           (multiple-value-bind (q r s)
+               (barycentric-coordinates (vec3 x y 1.0) a b c)
+             (unless (or (minusp q) (minusp r) (minusp s))
+               ;; inside triangle, so draw it
+               (let ((z (+ (* q (vz3 a))
+                           (* r (vz3 b))
+                           (* s (vz3 c)))))
+                 (when (> z (aref *z-buffer* x y))
+                   (setf (aref *z-buffer* x y) z)
+                   (set-pixel-color! image x y *draw-color*)))))))
+    (let ((min-x (truncate (min (vx3 a) (vx3 b) (vx3 c))))
+          (max-x (truncate (max (vx3 a) (vx3 b) (vx3 c))))
+          (min-y (truncate (min (vy3 a) (vy3 b) (vy3 c))))
+          (max-y (truncate (max (vy3 a) (vy3 b) (vy3 c)))))
+      (loop :for x :from min-x :upto max-x
+            :do (loop :for y :from min-y :upto max-y
+                      :do (draw-point x y))))))
 
 
-(defun set-pixel-color! (image col row color)
-  (setf (aref image row col 0) (elt color 0))
-  (setf (aref image row col 1) (elt color 1))
-  (setf (aref image row col 2) (elt color 2)))
+(defun set-pixel-color! (image x y color)
+  (let ((row (- (array-dimension image 0) y))
+        (col x))
+    (setf (aref image row col 0) (elt color 0))
+    (setf (aref image row col 1) (elt color 1))
+    (setf (aref image row col 2) (elt color 2))))
 
 
 ;;; Demo
@@ -85,6 +54,26 @@
         (floor (min (* g 256) 255))
         (floor (min (* b 256) 255))))
 
+(defun z-projection (z)
+  (mat  1 0      0   0
+        0 1      0   0
+        0 0      1   0
+        0 0 (/ -1 z) 1))
+
+(alexandria:define-constant +depth+ 255)
+
+(defun viewport (x y width height)
+  (mat (/ width 2)     0           0     (+ x (/ width 2))
+            0      (/ height 2)    0     (+ y (/ height 2))
+            0           0     (/ +depth+ 2)  (/ +depth+ 2)
+            0           0          0              1))
+
+(defun v4->v3 (v)
+  (with-vec (x y z w) v
+    (vec3 (/ x w)
+          (/ y w)
+          (/ z w))))
+
 (defun render-scene (file &key (display t) (width 800) (height 800))
   (let* ((png (make-instance 'png
                              :width (1+ width)
@@ -92,20 +81,13 @@
          (image (data-array png))
          (light-direction (vec3 0 0 -1))
          (obj (load-wavefront-object *object-path*)))
-    (flet ((project (u)
-             (let ((x (round (* (+ 1 (vx3 u))
-                                width
-                                0.5)))
-                   (y (round (* (+ 1 (vy3 u))
-                                height
-                                0.5))))
-               ;; NOTE: We reflect y coordinates.
-               (vec2 x (- height y)))))
-      (let ((*draw-color* (list 255 255 255))
-            (*z-buffer* (make-array (list (1+ width) (1+ height))
-                                    :element-type 'single-float
-                                    :initial-element most-negative-double-float))
-            (*world->screen* #'project))
+    (let ((*projection-matrix* (z-projection 3))
+          (*viewport-matrix* (viewport (* width 1/8) (* height 1/8)
+                                       (* width 3/4) (* height 3/4)))
+          (*z-buffer* (make-array (list (1+ width) (1+ height))
+                                  :element-type 'single-float
+                                  :initial-element most-negative-single-float)))
+      (let ((transform (m* *viewport-matrix* *projection-matrix*)))
         (loop :for (ia ib ic) :across (wavefront-object-faces obj)
               :for a := (elt (wavefront-object-vertices obj) ia)
               :for b := (elt (wavefront-object-vertices obj) ib)
@@ -116,7 +98,12 @@
                          (intensity (dot-product normal light-direction)))
                     (when (> intensity 0)
                       (let ((*draw-color* (color intensity intensity intensity)))
-                        (draw-triangle image a b c)))))))
+                        ;; get screen coordinates
+                        (let ((sa (v4->v3 (m* transform a)))
+                              (sb (v4->v3 (m* transform b)))
+                              (sc (v4->v3 (m* transform c))))
+                          (draw-triangle image sa sb sc))))))))
+
     (write-png png file)
     (when display
       (sb-ext:run-program "/usr/bin/open" (list file)))))
